5b7acb1d789630ccf6acbc685878e3b63c7c4050
Prahalad Belavadi <prahaladbelavadi@gmail.com>
2018-03-06 15:38:26 +0530

    proof of work

---
+++ proof-of-work.py
@@ -0,0 +1,10 @@
+from hashlib import sha256
+x = 5
+y = 0  # We don't know what y should be yet...
+while sha256(f'{x*y}'.encode()).hexdigest()[-1] != "0":
+    y += 1
+print(f'The solution is y = {y}')
+
+"""
+For x = 5 and difficulty = 0, y = 21
+"""
